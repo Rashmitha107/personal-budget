57432a52bf485f03c0e9f453426497b54e24743e
Rashmitha107 <rnagirim@uncc.edu>
2023-09-17 12:49:21 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,43 +4,14 @@ const port = 3000;
 
 app.use('/',express.static('public'))
 
-const budget = [
-    {
-        title: 'Eat out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 400
-    },
-    {
-        title: 'Groceries',
-        budget: 100
-    },
-    {
-        title: 'Commute',
-        budget: 100
-    },
-    {
-        title: 'Shopping',
-        budget: 100
-    },
-    {
-        title: 'Entertainment',
-        budget: 60
-    },
-    {
-        title: 'Insurance',
-        budget: 500
-    }
-]
-
 app.get('/hello',(req,res)=>{
     res.send('Hello world');
 });
 
-app.get('/budget',(req,res)=>{
-    res.json(budget);
+const myBudget = require("./budget.json");
+
+app.get('/Budget',(req,res)=>{
+    res.json(myBudget);
 });
 
 app.listen(port,()=>{
